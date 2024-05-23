262e785ffec09179b2c984d4d803080c2736b2e1
LF112 <lf@lf112.net>
2024-05-23 22:00:29 +0800

    fix: commit

--- src/app/components/devtools/commit.tsx
+++ src/app/components/devtools/commit.tsx
@@ -2,17 +2,19 @@
 import type React from 'react'
 
 const commitScript = (() => {
-  const commit = `
-______________  ________________
-___  ____/_  / / /__  __/___  _/
-__  /_   _  / / /__  /   __  /
-_  __/   / /_/ / _  /   __/ /
-/_/      \\____/  /_/    /___/
-⚡️ Written by TypeScript, Coding with Love.
- | Website: https://www.lf112.net
- | Github: https://github.com/LF112
- | E-Mail: lf@lf112.net
-`
+  const commit = [
+    '',
+    '______________  ________________',
+    '___  ____/_  / / /__  __/___  _/',
+    '__  /_   _  / / /__  /   __  /',
+    '_  __/   / /_/ / _  /   __/ /',
+    String.raw`/_/      \____/  /_/    /___/`,
+    '⚡️ Written by TypeScript, Coding with Love.',
+    ' | Website: https://www.lf112.net',
+    ' | Github: https://github.com/LF112',
+    ' | E-Mail: lf@lf112.net',
+    '',
+  ].join('\n')
   console.log(
     '\n %c \u26A1futi-detroit  %c https://www.lf112.net %c BY LF112 (@futiwolf) \n\n',
     'color:#fff;background:#0091e4;padding:5px 0;border-radius:4px 0 0 4px',
